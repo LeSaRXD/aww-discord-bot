c398e8031a15944e51de9158aad5df5fa8fbe443
LeSaR <ilkov2006@gmail.com>
2023-04-25 15:31:47 +0300

    Added custom status. Added the ability to mention the bot for statistics

--- bot.py
+++ bot.py
@@ -15,31 +15,13 @@ bot = discord.Client(intents=intents)
 
 
 
+counter = {}
+
 def is_aww(text):
 	text = [char for char in text.lower() if char in ascii_lowercase]
 	return all([char in ["a", "w"] for char in text]) and (list(text) == sorted(text)) and ("a" in text) and ("w" in text)
 
-@bot.event
-async def on_ready():
-	print(f"Logged in as {bot.user}")
-
-
-
-with open("counts.json", "r") as count_file:
-	try:
-		counter = json.load(count_file)
-	except:
-		counter = {}
-	print(counter)
-
-@bot.event
-async def on_message(message: discord.Message):
-	if message.author == bot.user:
-		return
-
-	if not is_aww(message.content):
-		return
-	
+async def aww(message):
 	counter[str(message.author.id)] = (count := counter.get(str(message.author.id), 0) + 1)
 	try:
 		with open("counts.json", "w") as count_file:
@@ -49,6 +31,39 @@ async def on_message(message: discord.Message):
 
 	await message.channel.send(f"<@{message.author.id}> said aww {count} time{'s' if count > 1 else ''}")
 
+def total_awws():
+	return sum(counter.values())
+
+async def update_status():
+	await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=3, name=f"{total_awws()} awws total"))
+
+
+@bot.event
+async def on_message(message: discord.Message):
+	if message.author == bot.user:
+		return
+
+	if is_aww(message.content):
+		await aww(message)
+		await update_status()
+
+	elif f"<@{bot.user.id}>" in message.content:
+		c = counter.get(str(message.author.id), 0)
+		await message.channel.send(f"<@{message.author.id}> said aww {c} time{'s' if c != 1 else ''}\nTotal awws: {total_awws()}")
+		
+@bot.event
+async def on_ready():
+	print(f"Logged in as {bot.user}")
+	await update_status()
+
+
+
+with open("counts.json", "r") as count_file:
+	try:
+		counter = json.load(count_file)
+	except:
+		print("error opening counts.json")
+	print(counter)
 try:
 	TOKEN = os.getenv("BOT_TOKEN")
 	if TOKEN:
